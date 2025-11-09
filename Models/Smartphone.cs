1ef08cb33df04074d48505839fbb5dafcd1817cd
Kelvyn <Kelvynandrew70@gmail.com>
2025-11-09 16:12:12 -0300

    Desafio Concluido com adição de um meto 'apresentar'

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,29 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero,string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Smartphone modelo: {Modelo}" + "\n"
+                             + $"Numero: {Numero}" + "\n"
+                             + $"Imei: {Imei}"+ "\n"
+                             + $"Memória: {Memoria}");
         }
 
         public void ReceberLigacao()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.Reflection.Metadata;
+using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia.");
+Smartphone nokia = new Nokia(numero: "23456-789", modelo: "Modelo 1", imei: "12345567899", memoria: 64);
+nokia.Apresentar();
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone.");
+Smartphone iphone = new Iphone(numero: "13456-789", modelo: "Modelo 4", imei: "1234556231", memoria: 128);
+iphone.Apresentar();
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
 // TODO: Realizar os testes com as classes Nokia e Iphone
